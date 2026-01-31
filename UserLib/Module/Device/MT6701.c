91a51084e00c4fc0609dfde8e18d3a6f76a2e89c
Ding Yong <2903964879@qq.com>
2026-02-01 06:03:01 +0800

    Initialize STM32 project with core files and configuration

---
+++ UserLib/Module/Device/MT6701.c
@@ -0,0 +1,4 @@
+//
+// Created by DingYong on 2026/2/1.
+//
+#include "MT6701.h"
---
+++ UserLib/Module/Device/MT6701.h
@@ -0,0 +1,7 @@
+//
+// Created by DingYong on 2026/2/1.
+//
+#ifndef MT6701_H
+#define MT6701_H
+
+#endif //MT6701_H
